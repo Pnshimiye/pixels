e9cb47433e7b030f5f907d59170fb2dc6e91bbca
Pnshimiye <pnshimiye@gmail.com>
2019-03-14 18:27:43 +0200

    add properties to the image model

--- myGallery/models.py
+++ myGallery/models.py
@@ -1,27 +1,32 @@
 from django.db import models
-
-class Image(models.Model):
-    name = models.CharField(max_length =30)
-    image = models.CharField(max_length =30)
-    description = models.CharField(max_length =30)
-    category = models.ForeignKey(Editor)
-    Location = models.ForeignKey(Editor)
-    capture_date = models.DateTimeField(auto_now_add=False)
-
-    def __str__(self):
-       return self.name
+import datetime as dt
 
 
-class category(models.Model):
+
+
+class Category(models.Model):
     name = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
-
-
 
 class Location(models.Model):
     name = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    image = models.CharField(max_length =30)
+    description = models.CharField(max_length =30)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+    capture_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return self.name
+
+    class Meta:
+      ordering = ['name']
+
+
