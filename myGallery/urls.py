500fbb0988eb595f291831139e7806e49e508e4f
Pnshimiye <pnshimiye@gmail.com>
2019-03-15 00:18:35 +0200

    add bootstrap

--- myGallery/urls.py
+++ myGallery/urls.py
@@ -1,6 +1,8 @@
+
 from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'home'),
+    url(r'^$',views.images_today,name='imagesToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastuploads')
 ]
--- pixels/urls.py
+++ pixels/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^myGallery/',include('myGallery.urls'))
+    url(r'',include('myGallery.urls'))
 ]
